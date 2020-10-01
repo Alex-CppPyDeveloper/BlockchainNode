1c8c657cb46e2db5ea9349f80c17886a41a2d957
Alex <alexej.eroshin@yandex.ru>
2020-10-01 12:35:11 +0300

    version 1.0

---
+++ BlockChainNode/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace BlockChainNode
+{
+
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string base_url = "https://rpc.tzkt.io/mainnet/";
+            string url = "chains/main/blocks/head/header";
+            СlassBlockchainNode classBlockchain = new СlassBlockchainNode(base_url);
+            Console.WriteLine("Hello World!");
+         
+            _ = new Head();
+            Head head = classBlockchain.GetHead(url);
+            Console.WriteLine(head.Chain_id);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BlockChainNode/СlassBlockchainNode.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+using System.Net;
+using System.IO;
+
+namespace BlockChainNode
+{
+    class Head
+    {
+        public string Chain_id { get; set; }
+        public string Hash { get; set; }
+        public string Predecessor { get; set; }
+        public int Level { get; set; }
+        public DateTime Timestamp { get; set; }
+
+
+    }
+    class СlassBlockchainNode
+    {
+        public string base_url;
+      public СlassBlockchainNode(string base_url)
+        {
+            this.base_url = base_url ;
+        }
+        public Head GetHead(string url)
+        
+        {
+            Console.WriteLine("Hello World!");
+            url = base_url + url;
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+
+            Stream stream = response.GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+            string s = sr.ReadToEnd();
+            Console.WriteLine(s);
+            _ = JObject.Parse(s);
+
+            Head parsed = JsonConvert.DeserializeObject<Head>(s);
+
+
+
+
+            Console.WriteLine($"Head.ChainId: {parsed.Chain_id}");
+            Console.WriteLine($"Head.Hash: {parsed.Hash}");
+            Console.WriteLine($"Head.Predecessor: {parsed.Predecessor}");
+            Console.WriteLine($"Head.Level: {parsed.Level}");
+            Console.WriteLine($"Head.Timestamp: {parsed.Timestamp}");
+
+            Console.ReadLine();
+
+            return parsed;
+
+        }
+    }
+
+   
+}
